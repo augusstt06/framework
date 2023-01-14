d2e1d24559a1ac167246a9ac25dcb1797ab2425f
augusstt06 <augusstt06@gmail.com>
2023-01-15 02:10:01 +0900

    Use useState

--- react/with_ts/src/component/create.tsx
+++ react/with_ts/src/component/create.tsx
@@ -1,35 +1,32 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
+import { type_todo } from "../type";
 
 // useState = 사용자가 입력할때마다 값을 받아온다
 // useRef = 사용자가 입력을 마치고 submit을 누를때 값을 받아온다.
 function Create() {
-  const todoDate = useRef<HTMLInputElement>(null);
-  const todoText = useRef<string>("");
-  const todoComplete = useRef<boolean>(false);
+  const [todoDate, setTodoDate] = useState<number>(0);
+  const [todoText, setTodoText] = useState<string>("");
+  const [todoComplete, setTodoComplete] = useState<boolean>(false);
 
-  const todoList = [];
+  const todos = Array<type_todo>;
 
-  const submitText = (
-    e: React.ChangeEvent<HTMLInputElement>,
-    text: React.MutableRefObject<string>
-  ) => {
-    const value = e.target.value;
-    text.current = value;
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    switch (e.target.name) {
+      case "text":
+        setTodoText(e.target.value);
+        break;
+      case "date":
+        setTodoDate(Number(e.target.value));
+        break;
+      default:
+        break;
+    }
   };
-
-  const registerTodo = () => {
-    console.log(todoText.current, "등록 완료");
-  };
-  console.log(todoText, "최초값");
+  //   const registerTodo = () => {};
   return (
     <div>
-      <input
-        placeholder="할 일을 입력하세요"
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          submitText(e, todoText);
-        }}
-      />
-      <button onClick={registerTodo}>등록</button>
+      <input placeholder="할 일을 입력하세요" name="text" />
+      <button>등록</button>
     </div>
   );
 }
