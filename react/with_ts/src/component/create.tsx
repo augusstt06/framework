5c46c53b0be870b61a744223778d31941499403a
augusstt06 <augusstt06@gmail.com>
2023-01-15 00:43:42 +0900

    type declaration

--- react/with_ts/src/component/create.tsx
+++ react/with_ts/src/component/create.tsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { todo } from "../type";
 
 function Create() {
-  return <div></div>;
+  const [todo, setTodo] = useState<todo>();
+
+  //   const typingTodo = (e:React.Event) => {
+
+  //   }
+
+  return (
+    <div>
+      <input placeholder="할 일을 입력하세요" />
+      <button>등록</button>
+    </div>
+  );
 }
 export default Create;
