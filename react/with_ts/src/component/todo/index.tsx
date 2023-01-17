cb4e5e312928ef9eaf7d27ec1fbe4bc7704dc1b6
augusstt06 <augusstt06@gmail.com>
2023-01-17 22:39:55 +0900

    component change

--- react/with_ts/src/component/todo/index.tsx
+++ react/with_ts/src/component/todo/index.tsx
@@ -1,13 +1,34 @@
-import React, { useState } from "react";
-import Create from "./each_func/create";
+import React, { FormEvent, useState } from "react";
+import { todolist } from "../../type";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import TodoList from "./each_func/todolist";
 
 // useState = 사용자가 입력할때마다 값을 받아온다
 // useRef = 사용자가 입력을 마치고 submit을 누를때 값을 받아온다.
+
 function Todo() {
   const [startDate, setStartDate] = useState<Date>(new Date());
+  const [todoText, setTodoText] = useState<string>("");
 
+  const handleText = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoText(e.target.value);
+  };
+
+  const [todolist, setTodolist] = useState<todolist[]>([]);
+
+  const registerTodo = (e: FormEvent<HTMLButtonElement>) => {
+    const newTodo = {
+      // 우선 변환만 시켜놓고 추후에 수정
+      date: String(startDate),
+      todolist: { text: todoText, complete: false },
+    };
+    setTodolist([...todolist, newTodo]);
+    setTodoText("");
+    setStartDate(new Date());
+    e.preventDefault();
+  };
+  console.log(todolist);
   return (
     <div>
       <div>
@@ -15,7 +36,14 @@ function Todo() {
           selected={startDate}
           onChange={(date: Date) => setStartDate(date)}
         />
-        <Create date={startDate} />
+        <input
+          placeholder="할 일을 입력하세요"
+          name="text"
+          value={todoText}
+          onChange={handleText}
+        />
+        <button onClick={registerTodo}>등록</button>
+        <TodoList todolist={todolist} setTodolist={setTodolist} />
       </div>
     </div>
   );
