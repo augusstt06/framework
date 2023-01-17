42f00773b48d484eae8f85a96f42b97ca16d4a8d
augusstt06 <augusstt06@gmail.com>
2023-01-17 22:40:34 +0900

    Create todolist.tsx

---
+++ react/with_ts/src/component/todo/each_func/todolist.tsx
@@ -0,0 +1,12 @@
+import React, { FormEvent } from "react";
+import { todolistProps } from "../../../type";
+
+// 받아와야 할것 : 할일의 각각 목록, 완료 여부, 날짜
+function TodoList(props: todolistProps) {
+  return (
+    <div>
+      <a>d</a>
+    </div>
+  );
+}
+export default TodoList;
