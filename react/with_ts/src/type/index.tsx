c50bf9c0e0b82ae089865a230d6c76dcbaeddfd0
augusstt06 <augusstt06@gmail.com>
2023-01-17 21:26:43 +0900

    Modify type.tsx

--- react/with_ts/src/type/index.tsx
+++ react/with_ts/src/type/index.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export interface todo_prop {
   text: string;
   complete: boolean;
@@ -6,4 +8,19 @@ export interface todo_prop {
 export interface todolist {
   date: string | undefined;
   todolist: todo_prop;
+}
+
+export interface checkbox {
+  complete?: boolean;
+  onClick?(): void;
+}
+
+export interface todoText {
+  complete?: boolean;
+  children: React.ReactNode;
+}
+
+export interface todolistProps {
+  setTodolist: React.Dispatch<React.SetStateAction<Array<todolist>>>;
+  todolist: todolist[];
 }
