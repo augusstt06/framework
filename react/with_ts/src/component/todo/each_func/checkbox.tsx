a73174add5ed18f3317ba7322cac01b1f881e5a5
augusstt06 <augusstt06@gmail.com>
2023-01-18 19:37:45 +0900

    Success checkbox individual check

--- react/with_ts/src/component/todo/each_func/checkbox.tsx
+++ react/with_ts/src/component/todo/each_func/checkbox.tsx
@@ -1,11 +1,18 @@
-import React, { ReactElement } from "react";
+import React from "react";
 import { checkbox } from "../../../type";
-import "../../../css/checkbox.css";
+// import "../../../css/checkbox.css";
 
-function Checkbox({ complete, onClick }: checkbox) {
+function Checkbox({ complete, completeHandler }: checkbox) {
   return (
-    <div className="container" onClick={onClick}>
-      <div className="checkIcon">{complete && "✔︎"}</div>
+    // <div className="container" onClick={onClick}>
+    //   <div className="checkIcon">{complete && "✔︎"}</div>
+    // </div>
+    <div>
+      <input
+        type="checkbox"
+        checked={complete}
+        onChange={(e: React.ChangeEvent) => completeHandler(e)}
+      />
     </div>
   );
 }
