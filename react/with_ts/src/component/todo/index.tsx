61c46961f8e06bc4beb985422042f632cfb6715f
augusstt06 <augusstt06@gmail.com>
2023-01-19 18:25:35 +0900

    Change arr to Set

--- react/with_ts/src/component/todo/index.tsx
+++ react/with_ts/src/component/todo/index.tsx
@@ -1,12 +1,15 @@
 import React, { FormEvent, useState } from "react";
-import { todolist } from "../../type";
+import { todolist_set } from "../../type";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import TodoList from "./each_func/todolist";
 
 // useState = 사용자가 입력할때마다 값을 받아온다
 // useRef = 사용자가 입력을 마치고 submit을 누를때 값을 받아온다.
-// todolist를 set으로 바꾸기
+
+// 완료된 할것에선 체크박스빼기
+// 완료되면 바로 렌더링 되게 바꾸기
+
 function Todo() {
   const [startDate, setStartDate] = useState<Date>(new Date());
   const [todoText, setTodoText] = useState<string>("");
@@ -15,31 +18,23 @@ function Todo() {
     setTodoText(e.target.value);
   };
 
-  const [todolist, setTodolist] = useState<todolist[]>([]);
-
-  const [completeTodo, setCompleteTodo] = useState(new Set());
-  console.log(completeTodo, "완료된 할것들");
-
+  const [completeTodo, setCompleteTodo] = useState(new Set<todolist_set>());
+  const [incompleteTodo, setIncompleteTodo] = useState(new Set<todolist_set>());
+  console.log(incompleteTodo, "안한거");
+  console.log(completeTodo, "한거");
+  const complete_arr = Array.from(completeTodo);
+  const incomplete_arr = Array.from(incompleteTodo);
   // set에 들어갈 객체 수정하기
-  const [newTodo, setNewTodo] = useState<todolist>({
-    date: "",
-    text: "",
-    complete: false,
-  });
+  // 삭제 버튼 만들기
 
-  const registerTodo = (e: FormEvent<HTMLButtonElement>) => {
+  const registerTodo_set = (e: FormEvent<HTMLButtonElement>) => {
     const newTodo = {
       // 우선 변환만 시켜놓고 추후에 수정 => 날짜 타입 수정
       date: String(startDate),
       text: todoText,
-      complete: false,
     };
-    setNewTodo({
-      date: String(startDate),
-      text: todoText,
-      complete: false,
-    });
-    setTodolist([...todolist, newTodo]);
+    incompleteTodo.add(newTodo);
+    setIncompleteTodo(incompleteTodo);
     setTodoText("");
     setStartDate(new Date());
     e.preventDefault();
@@ -58,19 +53,44 @@ function Todo() {
           value={todoText}
           onChange={handleText}
         />
-        <button onClick={registerTodo}>등록</button>
+
+        <button onClick={registerTodo_set}>등록</button>
       </div>
       <div></div>
-      {todolist.map((data) => (
-        <div key={data.date}>
-          <TodoList
-            date={data.date}
-            text={data.text}
-            completeTodo={completeTodo}
-            setCompleteTodo={setCompleteTodo}
-          />
-        </div>
-      ))}
+      <h2>완료되지 않은 할것들</h2>
+      {incomplete_arr && incomplete_arr.length !== 0 ? (
+        incomplete_arr.map((data) => (
+          <div key={data.date}>
+            <TodoList
+              date={data.date}
+              text={data.text}
+              incompleteTodo={incompleteTodo}
+              setIncompleteTodo={setIncompleteTodo}
+              completeTodo={completeTodo}
+              setCompleteTodo={setCompleteTodo}
+            />
+          </div>
+        ))
+      ) : (
+        <div>할 일을 등록해주세요</div>
+      )}
+      <h2>완료된 할것들</h2>
+      {complete_arr && complete_arr.length !== 0 ? (
+        complete_arr.map((data) => (
+          <div key={data.date}>
+            <TodoList
+              date={data.date}
+              text={data.text}
+              incompleteTodo={incompleteTodo}
+              setIncompleteTodo={setIncompleteTodo}
+              completeTodo={completeTodo}
+              setCompleteTodo={setCompleteTodo}
+            />
+          </div>
+        ))
+      ) : (
+        <div>할 일을 완료해주세요</div>
+      )}
     </div>
   );
 }
