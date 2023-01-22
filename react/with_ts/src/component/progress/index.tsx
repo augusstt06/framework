f9f64c56e7f18476bb44f6ae62480b5615483018
augusstt06 <augusstt06@gmail.com>
2023-01-22 21:39:52 +0900

    Apply Progressbar

--- react/with_ts/src/component/progress/index.tsx
+++ react/with_ts/src/component/progress/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { progressbar } from "../../type/index";
-import { CircularProgressbarWithChildren } from "react-circular-progressbar";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 // props로 받아오는 totalTodo는 상위컴포넌트의 incomoleteTodo + completeTodo  : 총 할 일의 갯수
 // 진도율로 보여줘야할 갯수 : completeTodo의 갯수
@@ -8,27 +9,44 @@ function Progress(props: progressbar) {
   const totalTodo = props.totalTodo;
   const completeTodo = props.completeTodo;
 
-  const completePercent = (completeTodo / totalTodo) * 100;
+  const completePercent = Math.floor((completeTodo / totalTodo) * 100);
 
   return (
     <div>
-      <CircularProgressbarWithChildren
+      <CircularProgressbar
         value={completePercent}
         text={
-          totalTodo === 0 ? "할 일을 입력해주세요" : `${completePercent}% 완료!`
+          totalTodo === 0
+            ? "할 일을\n 입력해주세요"
+            : `${completePercent}% 완료!`
         }
+        strokeWidth={20}
         styles={{
           root: {
-            display: "flex",
-            height: "100px",
-            width: "200px",
-            fontSize: "15px",
-            background: "beige",
+            height: "200px",
+          },
+          path: {
+            stroke: `rgba(62, 152, 199, ${completePercent / 100})`,
+            strokeLinecap: "butt",
+            transition: "stroke-dashoffset 0.5s ease 0s",
+            // transform: "rotate(0.25turn)",
+            transformOrigin: "center center",
+          },
+          trail: {
+            stroke: "#d6d6d6",
+            strokeLinecap: "butt",
+            transform: "rotate(0.25turn)",
+            transformOrigin: "center center",
+          },
+          text: {
+            fill: "#f88",
+            fontSize: "10px",
+          },
+          background: {
+            fill: "#3e98c7",
           },
         }}
-      >
-        <strong>test</strong>
-      </CircularProgressbarWithChildren>
+      />
     </div>
   );
 }
