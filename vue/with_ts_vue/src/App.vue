86eedf6bc6d2a65d9e436907a6ef61662ebc96e9
augusstt06 <augusstt06@gmail.com>
2023-01-26 22:15:36 +0900

    Apply Datepicker

--- vue/with_ts_vue/src/App.vue
+++ vue/with_ts_vue/src/App.vue
@@ -11,14 +11,18 @@
   <input placeholder="할일을 입력하세요" v-model="todoText" />
   <button @click="registerTodo">q버튼</button>
   <a>{{ todoText }}</a>
+  <div>
+    <a v-for="i in incomplete_arr" v-bind:key="i.date">{{ i }}</a>
+  </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
+import { todolist } from "./type";
 import DatePicker from "@vuepic/vue-datepicker";
 import "@vuepic/vue-datepicker/dist/main.css";
 
-// incompleteArr은 변수로 넣기
+// incompleteArr 어떻게 할까
 @Options({
   components: { DatePicker },
   data() {
@@ -40,17 +44,20 @@ import "@vuepic/vue-datepicker/dist/main.css";
         text: this.todoText,
       };
       this.incompleteTodo.add(newTodo);
+      this.incompleteTodo_arr.push(newTodo);
+      console.log(this.incompleteTodo_arr);
+      console.log(typeof this.incompleteTodo_arr);
     },
   },
 })
 export default class App extends Vue {
   // !는 해당 변수에 값이 무조건 할당되어있다는 뜻
-
   todoText!: string;
   startDate!: string;
   rerender!: boolean;
   completeTodo?: Set<any>;
   registerTodo!: () => void;
+  incomplete_arr!: Array<todolist>;
 }
 </script>
 
