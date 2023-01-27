d5fe25f37ebcec036af2b9ea43ae43f926d2b468
augusstt06 <augusstt06@gmail.com>
2023-01-27 21:57:33 +0900

    Create Checkbox.vue

---
+++ vue/with_ts_vue/src/components/Checkbox.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!-- <input
+      type="checkbox"
+      v-model="agreement"
+      true-value="yes"
+      false-value="no"
+    /> -->
+    <input type="checkbox" :value="complete" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  components: {},
+  props: {
+    complete: Boolean,
+  },
+})
+export default class Checkbox extends Vue {
+  complete?: boolean;
+}
+</script>
