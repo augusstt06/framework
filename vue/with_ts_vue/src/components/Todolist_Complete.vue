557f280a0d40682b571a946837bdc364799e8bd3
augusstt06 <augusstt06@gmail.com>
2023-01-29 23:07:03 +0900

    Todolist_Complete.vue

---
+++ vue/with_ts_vue/src/components/Todolist_Complete.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>완료 안한거</div>
+</template>
+<script>
+import { Options, Vue } from "vue-class-component";
+@Options({
+  components: {},
+})
+export default class Todolist_Incomplete extends Vue {}
+</script>
